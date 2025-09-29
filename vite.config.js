2b54a3f06fa490bb7921621da7ddcbe6bdb91997
Kykkos <cyrrus.leka@gmail.com>
2025-09-29 18:15:22 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Explicitly specify index.html as the entry point
+    },
+  },
 });
